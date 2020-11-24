23a41807c82318cfeecc60a1d6c3c927e3f954ff
andrey.shevchenko <shevadon01@gmail.com>
2020-11-24 18:08:08 +0200

    started implemented showing favorites meals functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import SearchInput from "./SearchInput";
-
+import {RiUserStarFill} from 'react-icons/ri';
 
 const HeaderWrapper = styled('div')`
     padding: 10px 20px;
     border-bottom: 2px solid #cfa4bf;
     box-shadow: 0px 5px 7px #c571b76b;
     margin-bottom: 25px;
+    
+    & svg.favorite-icon{
+        font-size: 25px;
+        margin-left: 30px;
+        cursor: pointer;
+    }
 `;
 
 const Header = () => {
@@ -15,6 +21,7 @@ const Header = () => {
     return(
         <HeaderWrapper className={"header"}>
             <SearchInput/>
+            <RiUserStarFill className={"favorite-icon"}/>
         </HeaderWrapper>
     );
 };
